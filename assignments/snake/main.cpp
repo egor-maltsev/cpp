abe33da1eb1668d9d4d0a80657bbf59532b57a1d
Egor <86186716+egor-maltsev@users.noreply.github.com>
2024-11-03 00:01:27 +0300

    Add files via upload

--- assignments/snake/main.cpp
+++ assignments/snake/main.cpp
@@ -1,3 +1,10 @@
+//
+//  main.cpp
+//  snake
+//
+//  Created by Егор Мальцев on 29.10.2024.
+//
+
 /*
  * Для компиляции необходимо добавить ключ -lncurses
  * g++ -o snake main.cpp -lncurses
@@ -8,6 +15,9 @@
 #include <ctime>
 #include <cstdlib>
 #include <ncurses.h>
+#include <thread>
+#include <mutex>
+#include <atomic>
 
 enum {LEFT=1, UP, RIGHT, DOWN, STOP_GAME='q'};
 enum {MAX_TAIL_SIZE=1000, START_TAIL_SIZE=3, MAX_FOOD_SIZE=20, FOOD_EXPIRE_SECONDS=10, SPEED=20000, SEED_NUMBER=3};
@@ -27,58 +37,44 @@ class Snake {
 public:
     int x, y, direction;
     size_t tsize;
+    char headChar, tailChar;
     std::vector<Tail> tail;
 
-    Snake() : x(0), y(2), direction(RIGHT), tsize(START_TAIL_SIZE+1) {
+    Snake(int startX, int startY, int startDirection, char headCh, char tailCh)
+        : x(startX), y(startY), direction(startDirection), tsize(START_TAIL_SIZE+1), headChar(headCh), tailChar(tailCh) {
         tail.resize(MAX_TAIL_SIZE);
     }
 
-    void move() {
+    void move(std::mutex& mtx) {
         int max_x = 0, max_y = 0;
-        char ch = '@';
         getmaxyx(stdscr, max_y, max_x);
+
+        std::lock_guard<std::mutex> lock(mtx);
         mvprintw(y, x, " ");
 
         switch (direction) {
-            case LEFT:
-                if (x <= 0) x = max_x;
-                mvprintw(y, --x, "%c", ch);
-                break;
-            case RIGHT:
-                if (x >= max_x) x = 0;
-                mvprintw(y, ++x, "%c", ch);
-                break;
-            case UP:
-                if (y <= 0) y = max_y;
-                mvprintw(--y, x, "%c", ch);
-                break;
-            case DOWN:
-                if (y >= max_y) y = 0;
-                mvprintw(++y, x, "%c", ch);
-                break;
+            case LEFT:  x = (x > 0) ? x - 1 : max_x; break;
+            case RIGHT: x = (x < max_x) ? x + 1 : 0; break;
+            case UP:    y = (y > 0) ? y - 1 : max_y; break;
+            case DOWN:  y = (y < max_y) ? y + 1 : 0; break;
         }
+        mvprintw(y, x, "%c", headChar);
         refresh();
     }
 
-    void moveTail() {
-        char ch = '*';
+    void moveTail(std::mutex& mtx) {
+        std::lock_guard<std::mutex> lock(mtx);
         mvprintw(tail[tsize - 1].y, tail[tsize - 1].x, " ");
         for (size_t i = tsize - 1; i > 0; i--) {
             tail[i] = tail[i - 1];
-            if (tail[i].y || tail[i].x) {
-                mvprintw(tail[i].y, tail[i].x, "%c", ch);
-            }
+            mvprintw(tail[i].y, tail[i].x, "%c", tailChar);
         }
         tail[0].x = x;
         tail[0].y = y;
     }
 
     void addTail() {
-        if (tsize < MAX_TAIL_SIZE) {
-            tsize++;
-        } else {
-            mvprintw(0, 0, "Can't add tail");
-        }
+        if (tsize < MAX_TAIL_SIZE) tsize++;
     }
 
     bool isCrash() {
@@ -89,122 +85,108 @@ public:
     }
 };
 
-void changeDirection(int& new_direction, int key) {
-    switch (key) {
-        case KEY_DOWN: new_direction = DOWN; break;
-        case KEY_UP: new_direction = UP; break;
-        case KEY_LEFT: new_direction = LEFT; break;
-        case KEY_RIGHT: new_direction = RIGHT; break;
+class Game {
+public:
+    Snake snake1, snake2;
+    std::vector<Food> food;
+    std::mutex mtx;
+    std::atomic<bool> running;
+
+    Game() : snake1(0, 2, RIGHT, '@', '*'), snake2(5, 5, LEFT, '#', '+'), food(MAX_FOOD_SIZE), running(true) {
+        srand(time(nullptr));
+        initFood();
     }
-}
 
-void initFood(std::vector<Food>& food) {
-    int max_y = 0, max_x = 0;
-    getmaxyx(stdscr, max_y, max_x);
-    for (auto& f : food) {
-        f = {0, 0, 0, '$', false};
+    void initFood() {
+        for (auto& f : food) {
+            f = {0, 0, 0, '$', false};
+        }
     }
-}
 
-void putFoodSeed(Food& fp) {
-    int max_x = 0, max_y = 0;
-    getmaxyx(stdscr, max_y, max_x);
-    mvprintw(fp.y, fp.x, " ");
-    fp.x = rand() % (max_x - 1);
-    fp.y = rand() % (max_y - 2) + 1;
-    fp.put_time = time(nullptr);
-    fp.enable = true;
-    mvprintw(fp.y, fp.x, "%c", fp.point);
-}
+    void putFoodSeed(Food& fp) {
+        int max_x = 0, max_y = 0;
+        getmaxyx(stdscr, max_y, max_x);
 
-void putFood(std::vector<Food>& food) {
-    for (auto& f : food) {
-        putFoodSeed(f);
+        std::lock_guard<std::mutex> lock(mtx);
+        mvprintw(fp.y, fp.x, " ");
+        fp.x = rand() % (max_x - 1);
+        fp.y = rand() % (max_y - 2) + 1;
+        fp.put_time = time(nullptr);
+        fp.enable = true;
+        mvprintw(fp.y, fp.x, "%c", fp.point);
     }
-}
 
-void refreshFood(std::vector<Food>& food) {
-    for (auto& f : food) {
-        if (f.put_time && (!f.enable || (time(nullptr) - f.put_time) > FOOD_EXPIRE_SECONDS)) {
+    void putFood() {
+        for (auto& f : food) {
             putFoodSeed(f);
         }
     }
-}
 
-bool haveEat(Snake& snake, std::vector<Food>& food) {
-    for (auto& f : food) {
-        if (f.enable && snake.x == f.x && snake.y == f.y) {
-            f.enable = false;
-            return true;
-        }
-    }
-    return false;
-}
-
-void repairSeed(std::vector<Food>& food, Snake& snake) {
-    for (size_t i = 0; i < snake.tsize; i++) {
+    bool haveEat(Snake& snake) {
         for (auto& f : food) {
-            if (f.x == snake.tail[i].x && f.y == snake.tail[i].y && f.enable) {
+            if (f.enable && snake.x == f.x && snake.y == f.y) {
+                f.enable = false;
                 putFoodSeed(f);
+                return true;
             }
         }
+        return false;
     }
-}
 
-void printLevel(Snake& snake) {
-    int max_x = 0, max_y = 0;
-    getmaxyx(stdscr, max_y, max_x);
-    mvprintw(0, max_x - 10, "LEVEL: %zu", snake.tsize);
-}
+    void play(Snake& snake) {
+        while (running) {
+            snake.move(mtx);
+            snake.moveTail(mtx);
 
-void printExit(Snake& snake) {
-    int max_x = 0, max_y = 0;
-    getmaxyx(stdscr, max_y, max_x);
-    mvprintw(max_y / 2, max_x / 2 - 5, "Your LEVEL is %zu", snake.tsize);
-}
+            if (haveEat(snake)) snake.addTail();
+            if (snake.isCrash()) running = false;
 
-int main() {
-    srand(time(nullptr));
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
 
-    Snake snake;
-    std::vector<Food> food(MAX_FOOD_SIZE);
-    initFood(food);
+    void start() {
+        initscr();
+        keypad(stdscr, TRUE);
+        raw();
+        noecho();
+        curs_set(FALSE);
+        putFood();
 
-    initscr();
-    keypad(stdscr, TRUE);
-    raw();
-    noecho();
-    curs_set(FALSE);
-    
-    mvprintw(0, 0, "  Use arrows for control. Press 'q' for EXIT");
-    putFood(food);
-    timeout(0);
+        std::thread t1(&Game::play, this, std::ref(snake1));
+        std::thread t2(&Game::play, this, std::ref(snake2));
 
-    int key_pressed = 0;
-    while (key_pressed != STOP_GAME) {
-        key_pressed = getch();
-        changeDirection(snake.direction, key_pressed);
-        
-        if (snake.isCrash()) break;
+        int key_pressed = 0;
+        while (running && key_pressed != STOP_GAME) {
+            key_pressed = getch();
 
-        snake.move();
-        snake.moveTail();
-
-        if (haveEat(snake, food)) {
-            snake.addTail();
-            printLevel(snake);
+            std::lock_guard<std::mutex> lock(mtx);
+            changeDirection(snake1.direction, key_pressed);
+            
+            if (key_pressed == 'w') snake2.direction = UP;
+            else if (key_pressed == 'a') snake2.direction = LEFT;
+            else if (key_pressed == 's') snake2.direction = DOWN;
+            else if (key_pressed == 'd') snake2.direction = RIGHT;
         }
 
-        refreshFood(food);
-        repairSeed(food, snake);
-
-        timeout(100);
+        running = false;
+        t1.join();
+        t2.join();
+        endwin();
     }
 
-    printExit(snake);
-    timeout(SPEED);
-    getch();
-    endwin();
+    void changeDirection(int& direction, int key) {
+        switch (key) {
+            case KEY_DOWN: direction = DOWN; break;
+            case KEY_UP: direction = UP; break;
+            case KEY_LEFT: direction = LEFT; break;
+            case KEY_RIGHT: direction = RIGHT; break;
+        }
+    }
+};
 
+int main() {
+    Game game;
+    game.start();
     return 0;
 }
